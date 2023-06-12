6e2c598fec0f53562bce0eb25fc3e233b5043748
Artem_Lakatun <alakatuni@mail.ru>
2023-06-12 16:08:00 +0300

    add: new directory index files, html file(tournaments)

--- file.js
+++ file.js
@@ -200,3 +200,17 @@ document.addEventListener("DOMContentLoaded", function () {
         navMenu.classList.toggle("active");
     });
 });
+
+//плавный якорь навигации
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for(let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
