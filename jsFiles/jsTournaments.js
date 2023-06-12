fcf4856158f71c9fd9a07c7cdf28c3c1a57e2716
Artem_Lakatun <alakatuni@mail.ru>
2023-06-12 21:09:21 +0300

    feat: complete  block pictures, add block contacts(10%)

--- jsFiles/jsTournaments.js
+++ jsFiles/jsTournaments.js
@@ -23,16 +23,34 @@ const points = [
     {x :1100, y: 300},
     {x :1150, y: 390},
     {x :1150, y: 520},
-    {x :1350, y: 580},
-    {x :1350, y: 730},
-    {x :1150, y: 790},
-    {x :590, y: 890},
+    {x :1300, y: 580},
+    {x :1400, y: 730},
+    {x :1350, y: 790},
+    {x :990, y: 890},
     {x :550, y: 850},
     {x :250, y: 990},
     {x :150, y: 1100},
     {x :160, y: 1400},
     {x :390, y: 1400},
     {x :450, y: 1500},
+    {x :650, y: 1700},
+    {x :800, y: 1700},
+    {x :950, y: 1550},
+    {x :1150, y: 1150},
+    {x :1200, y: 1400},
+    {x :1400, y: 1800},
+    {x :1200, y: 1950},
+    {x :980, y: 2050},
+    {x :500, y: 2050},
+    {x :350, y: 1950},
+    {x :310, y: 2000},
+    {x :310, y: 2150},
+    {x :310, y: 2650},
+    {x :400, y: 2700},
+    {x :800, y: 2670},
+    {x :1000, y: 2650},
+    {x :1000, y: 2350},
+    {x :1200, y: 2340},
 
 ];
 ctx.beginPath();
@@ -45,7 +63,7 @@ for (let i = 1; i < points.length - 1; i++) {
     const controlY = currentPoint.y;
     const endX = nextPoint.x;
     const endY = nextPoint.y;
-    ctx.arcTo(controlX, controlY, endX, endY, 45); // Измените радиус дуги по своему усмотрению
+    ctx.arcTo(controlX, controlY, endX, endY, 45); // Изменяем радиус дуги по своему усмотрению
 }
 ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
 ctx.stroke();
